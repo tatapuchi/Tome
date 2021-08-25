2f70c5c0726ddc26e65d8db0f9f3e6034fa69e3d
Vibhor Singh <vibdaddy12@gmail.com>
2021-08-25 22:04:07 +0200

    Setup base models

---
+++ Tome.Models/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tome.Models.Models
+{
+    public class Book : ModelBase 
+    {
+    }
+}
---
+++ Tome.Models/Models/ModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tome.Models.Models
+{
+    public class ModelBase
+    {
+        public int Id { get; set; }
+
+
+    }
+}
